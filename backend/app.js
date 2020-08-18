7445c0bda640ee48a4a71f7206a5729cd7bd37da
ChaChing3579 <kalanryan@gmail.com>
2020-08-18 01:37:22 -0400

    First Backend Commit

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,50 @@
-const express = require('express') 
-const app = express() 
-const port = 3000  
+// requirements
+const express = require('express');
+const app = express();
+const port = 3000;
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+// trackers
+var allClients = [];
+var activePlayers = [];
+
+// setup
+app.use(express.static('.'));
 
 app.get('/', (req, res) => {   
-	res.send('Hello World!') 
-})  
+	res.send('Hello World!');
+});
 
+// everything socket io related
+io.on('connection', (socket) => {
+
+	// this is ideally triggerd on connection after the user has entered a username
+	socket.on('addUser', (userName) => {
+		socket.userName = userName;
+		allClients.push(userName);
+
+		if (!activePlayers.length >= 2){
+			activePlayers.push(userName);
+		}
+
+	});
+
+	socket.on('testingMessage', (someStuff) => {
+		io.emit('testingMessage', someStuff);
+	});
+
+	// handles disconnects
+	socket.on('disconnect', () => {
+		allClients.splice(allClients.indexOf(socket.userName), 1);
+		if (activePlayers.indexOf(socket.userName) != -1) {
+			activePlayers.splice(allClients.indexOf(socket.userName));
+		}
+	});
+
+});
+
+// startup
 app.listen(port, () => {   
-	console.log(`Example app listening at http://localhost:${port}`) 
-})
+	console.log(`Example app listening at http://localhost:${port}`);
+});
