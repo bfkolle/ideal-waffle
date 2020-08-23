c0542f5db9d3cf76139bf90088bda3653a5d4c63
Brandon K <brandon.kolle@thetriangle.org>
2020-08-23 19:36:34 -0400

    Whole lotta lint fixes

--- frontend/src/app/models/gameLogic.ts
+++ frontend/src/app/models/gameLogic.ts
@@ -26,8 +26,8 @@ export default class GameLogic {
         }
 
         if (checkValidation) {
-            if(this.moveCausesCheck(piece, board, yValOld, xValOld, yValNew, xValNew)) {
-                console.log("Can't place yourself in check");
+            if (this.moveCausesCheck(piece, board, yValOld, xValOld, yValNew, xValNew)) {
+                console.log('Can\'t place yourself in check');
                 return false;
             }
         }
@@ -40,22 +40,23 @@ export default class GameLogic {
                     // This deals with non-capturing, only move 1-2 spaces forward
                     if (xValOld === xValNew) {
                         // Black pawns moving 2 spaces
-                        if(piece.color == 'black' && yValOld == 6 && yValNew == 4 && board[5][xValOld].piece == undefined){
+                        if (piece.color == 'black' && yValOld == 6 && yValNew == 4 && board[5][xValOld].piece == undefined){
                             board[yValOld - 1][xValNew].isEnPassant = true;
                             return true;
                         }
 
                         // White pawns moving 2 spaces
-                        if(piece.color == 'white' && yValOld == 1 && yValNew == 3 && board[2][xValOld].piece == undefined){
+                        if (piece.color == 'white' && yValOld == 1 && yValNew == 3 && board[2][xValOld].piece == undefined){
                             board[yValNew - 1][xValNew].isEnPassant = true;
                             return true;
                         }
 
                         // THIS DOESNT TAKE INTO ACCOUNT PAWN CAPTURING DIAGONALLY
-                        if(Math.abs(yValOld-yValNew)==1) {
-                            if(board[yValNew][xValNew].piece != undefined)
+                        if (Math.abs(yValOld - yValNew) === 1) {
+                            if (board[yValNew][xValNew].piece !== undefined) {
                                 return false;
-                            if(board[yValOld - 1][xValNew].isEnPassant == true) {
+                            }
+                            if (board[yValOld - 1][xValNew].isEnPassant === true) {
                                 board[yValOld - 1][xValNew].isEnPassant = false;
                             }
                             else if (board[yValOld + 1][xValNew].isEnPassant === true) {
@@ -104,39 +105,43 @@ export default class GameLogic {
                 break;
             }
             case 'rook': {
-                if(yValOld == yValNew)
+                if (yValOld == yValNew)
                 {
                     if (xValNew - xValOld > 0)
                     {
                         for (let i: number = xValOld + 1; i < xValNew; i++) {
-                            if(board[yValOld][i].piece != undefined)
+                            if (board[yValOld][i].piece != undefined) {
                                 return false;
+                            }
                         }
                     }
                     else
                     {
                         for (let i: number = xValOld - 1; i > xValNew; i--) {
-                            if(board[yValOld][i].piece != undefined)
+                            if (board[yValOld][i].piece != undefined) {
                                 return false;
+                            }
                         }
                     }
-                    
+
                     return true;
                 }
-                else if(xValOld == xValNew)
+                else if (xValOld == xValNew)
                 {
                     if (yValNew - yValOld > 0)
                     {
                         for (let i: number = yValOld + 1; i < yValNew; i++) {
-                            if(board[i][xValOld].piece != undefined)
+                            if (board[i][xValOld].piece != undefined) {
                                 return false;
+                            }
                         }
                     }
                     else
                     {
                         for (let i: number = yValOld - 1; i > yValNew; i--) {
-                            if(board[i][xValOld].piece != undefined)
+                            if (board[i][xValOld].piece != undefined) {
                                 return false;
+                            }
                         }
                     }
 
@@ -150,26 +155,28 @@ export default class GameLogic {
 
                 if (xDelta === yDelta)
                 {
-                    const xVal = xValOld;
-                    
-                    if(xValNew - xValOld > 0)
+                    let xVal = xValOld;
+
+                    if (xValNew - xValOld > 0)
                     {
                         if (yValNew - yValOld > 0)
                         {
                             for (let i: number = yValOld + 1; i < yValNew; i++) {
-                                if(board[i][xVal].piece != undefined)
+                                if (board[i][xVal].piece != undefined) {
                                     return false;
-                                
-                                    xVal++;
+                                }
+
+                                xVal++;
                             }
                         }
                         else
                         {
                             for (let i: number = yValOld - 1; i > yValNew; i--) {
-                                if(board[i][xVal].piece != undefined)
+                                if (board[i][xVal].piece != undefined) {
                                     return false;
+                                }
 
-                                    xVal++;
+                                xVal++;
                             }
                         }
                     }
@@ -178,19 +185,21 @@ export default class GameLogic {
                         if (yValNew - yValOld > 0)
                         {
                             for (let i: number = yValOld + 1; i < yValNew; i++) {
-                                if(board[i][xVal].piece != undefined)
+                                if (board[i][xVal].piece != undefined) {
                                     return false;
-                                
-                                    xVal--
+                                }
+
+                                xVal--;
                             }
                         }
                         else
                         {
                             for (let i: number = yValOld - 1; i > yValNew; i--) {
-                                if(board[i][xVal].piece != undefined)
+                                if (board[i][xVal].piece != undefined) {
                                     return false;
+                                }
 
-                                    xVal--
+                                xVal--;
                             }
                         }
                     }
@@ -225,19 +234,19 @@ export default class GameLogic {
     }
 
     static moveCausesCheck(piece: Piece, board: BoardTile[][], yValOld: number, xValOld: number, yValNew: number, xValNew: number): boolean {
-        if(this.isValidMove(piece, board, yValOld, xValOld, yValNew, xValNew, false)) {
-            let tempPiece: Piece = board[yValNew][xValNew].piece;
+        if (this.isValidMove(piece, board, yValOld, xValOld, yValNew, xValNew, false)) {
+            const tempPiece: Piece = board[yValNew][xValNew].piece;
             board[yValNew][xValNew].piece = board[yValOld][xValOld].piece;
             board[yValOld][xValOld].piece = undefined;
-            
-            let color: string = board[yValNew][xValNew].piece.color;
-            let kingCoords: number[] = this.findKingLocation(color, board);
-            let xValKing: number = kingCoords[0];
-            let yValKing: number = kingCoords[1];
 
-            for (let i: number = 0; i < 8; i++) {
-                for (let j: number = 0; j < 8; j++) {
-                    let temp2Piece: Piece = board[i][j].piece;
+            const color: string = board[yValNew][xValNew].piece.color;
+            const kingCoords: number[] = this.findKingLocation(color, board);
+            const xValKing: number = kingCoords[0];
+            const yValKing: number = kingCoords[1];
+
+            for (let i = 0; i < 8; i++) {
+                for (let j = 0; j < 8; j++) {
+                    const temp2Piece: Piece = board[i][j].piece;
 
                     if (temp2Piece != undefined && temp2Piece.color != color && this.isValidMove(temp2Piece, board, i, j, yValKing, xValKing, true)) {
                         board[yValOld][xValOld].piece = board[yValNew][xValNew].piece;
@@ -246,7 +255,7 @@ export default class GameLogic {
                     }
                 }
             }
-            
+
             board[yValOld][xValOld].piece = board[yValNew][xValNew].piece;
             board[yValNew][xValNew].piece = tempPiece;
             return false;
@@ -256,9 +265,9 @@ export default class GameLogic {
     }
 
     static findKingLocation(color: string, board: BoardTile[][]): [number, number] {
-        for (let i: number = 0; i < 8; i++) {
-            for (let j: number = 0; j < 8; j++) {
-                let tempPiece: Piece = board[i][j].piece;
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                const tempPiece: Piece = board[i][j].piece;
 
                 if (tempPiece != undefined && tempPiece.color === color && tempPiece.type === 'king') {
                     return [j, i];
